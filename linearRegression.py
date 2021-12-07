af3890ed8b6143021ea16d7323bba664a467cf77
Austin Peng <austinpeng2002@gmail.com>
2021-12-06 20:31:25 -0500

    reprocess data and high acc for linear regression and rf

--- linearRegression.py
+++ linearRegression.py
@@ -1,6 +1,6 @@
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression 
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 import pandas as pd
 import numpy as np
@@ -14,16 +14,16 @@ def linReg(trainDf, testDf2):
     test_targets = testDf2.loc[:, ['price']].values
     test_feats = testDf2.loc[:, features].values
     test_feats = StandardScaler().fit_transform(test_feats)
-    
+
     features = ['train_A']
     train_targets = trainDf.loc[:, ['price']].values
     train_feats = trainDf.loc[:, features].values
     train_feats = StandardScaler().fit_transform(train_feats)
-    
+
     clf = LinearRegression(normalize=True)
     clf.fit(train_feats, train_targets)
     y_pred = clf.predict(test_feats)
-    r2 = r2_score(test_targets,y_pred)
+    r2 = r2_score(test_targets, y_pred)
 
     normX = test_targets/np.linalg.norm(test_targets)
     normY = y_pred/np.linalg.norm(y_pred)
@@ -32,19 +32,18 @@ def linReg(trainDf, testDf2):
 
     print(mse)
     print(r2)
-  
+
     x = test_targets[0]
     y = y_pred[0]
     m, b = np.polyfit(x, y, 1)
     y = r2 * test_targets + b
 
-    plt.plot(test_targets, y_pred, 'o', color= "blue", label = "")
-    plt.plot(test_targets, y, color = "black")
+    plt.plot(test_targets, y_pred, 'o', color="blue", label="")
+    plt.plot(test_targets, y, color="black")
     plt.xlabel("Actual Prices")
     plt.ylabel("Predicted Prices")
     plt.show()
 
-.0000267
 
 if __name__ == '__main__':
     trainDf, testDf2 = dimRed()
--- myPCA.py
+++ myPCA.py
@@ -2,7 +2,7 @@ import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression 
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 import pandas as pd
 import numpy as np
@@ -10,16 +10,18 @@ import csv
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, r2_score
 
+
 def dimRed():
     csvFile = []
     data = []
-    with open('clean_data.csv', mode ='r', encoding = 'UTF-8') as file:     
-        csvFile = csv.reader(file) 
+    with open('clean_data.csv', mode='r', encoding='UTF-8') as file:
+        csvFile = csv.reader(file)
         count = 0
         for line in csvFile:
+            print(count)
             if count != 0:
                 data.append(line)
-            count+=1
+            count += 1
     data = np.array(data)
     data = np.float32(data)
 
@@ -30,12 +32,20 @@ def dimRed():
     # IQR = Q3 - Q1
     # df = df[~((df[cols] < Q1 - 1.5 * IQR)) | (df[cols] > (Q3+ 1.5))]
 
-    training_data, testing_data = train_test_split(data, test_size=0.2, random_state=25)
+    # data = pd.read_csv("clean_data.csv")
+    training_data, testing_data = train_test_split(
+        data, test_size=0.2, random_state=25)
 
-    dataDf = pd.DataFrame(training_data, columns = ['id','views', 'price', 'space', 'rooms', 'latitude', 'longitude'])
-    testDataDf = pd.DataFrame(testing_data, columns = ['id','views', 'price', 'space', 'rooms', 'latitude', 'longitude'])
+    dataDf = training_data
+    testDataDf = testing_data
+    dataDf = pd.DataFrame(training_data, columns=[
+                          'price', 'space', 'room', 'bedroom', 'furniture', 'latitude', 'longitude'])
+    testDataDf = pd.DataFrame(testing_data, columns=[
+                              'price', 'space', 'room', 'bedroom', 'furniture', 'latitude', 'longitude'])
 
-    features = ['views', 'space', 'rooms', 'latitude', 'longitude']
+    features = ['space', 'room', 'bedroom',
+                'furniture', 'latitude', 'longitude']
+    # features = [x for x in dataDf.columns if "price" not in x]
     feats = dataDf.loc[:, features].values
     feats = StandardScaler().fit_transform(feats)
 
@@ -45,15 +55,16 @@ def dimRed():
     pca = PCA(n_components=2)
     principalComponents = pca.fit_transform(feats)
     testPC = pca.fit_transform(testFeats)
-    
-    print(np.cumsum(pca.explained_variance_ / np.sum(pca.explained_variance_))[0])
 
-    df = pd.DataFrame(principalComponents, columns = ['train_A', 'train_B'])
-    testDf = pd.DataFrame(testPC, columns = ['test_A', 'train_B'])
+    print(np.cumsum(pca.explained_variance_ /
+          np.sum(pca.explained_variance_))[0])
 
-    df.to_csv('train_data.csv', index = None)
-    testDf.to_csv('test_data.csv', index = None)
-    
-    trainDf = pd.concat([df, dataDf[["price"]]], axis = 1)
-    testDf2 = pd.concat([testDf, testDataDf[["price"]]], axis = 1)
+    df = pd.DataFrame(principalComponents, columns=['train_A', 'train_B'])
+    testDf = pd.DataFrame(testPC, columns=['test_A', 'train_B'])
+
+    df.to_csv('train_data.csv', index=None)
+    testDf.to_csv('test_data.csv', index=None)
+
+    trainDf = pd.concat([df, dataDf[["price"]]], axis=1)
+    testDf2 = pd.concat([testDf, testDataDf[["price"]]], axis=1)
     return trainDf, testDf2
