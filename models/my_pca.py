e18fbf25484078f6bbf97fc33019167156af0dad
Austin Peng <austinpeng2002@gmail.com>
2021-12-21 11:58:50 -0800

    project completed.

--- models/my_pca.py
+++ models/my_pca.py
@@ -1,8 +1,6 @@
-import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 import pandas as pd
 import numpy as np
